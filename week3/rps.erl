b2de7d0ce636fff3a09babac3774c7cd2d0a4ef0
Hynek Vychodil <vychodil.hynek@gmail.com>
2017-03-27 08:27:07 +0200

    Rock, Paper, Scissors WIP continues

--- week3/rps.erl
+++ week3/rps.erl
@@ -8,6 +8,8 @@
 
 -export([choose/0, choose/1, freq/1]).
 
+-export([play/1, get_move/0]).
+
 possibilities() -> [rock, paper, scissors].
 
 beats(rock)     -> scissors;
@@ -27,6 +29,10 @@ points(X, Y) ->
 result2points(win)  -> 1;
 result2points(draw) -> 0;
 result2points(lose) -> -1.
+
+points2result(0) -> draw;
+points2result(X) when X > 0 -> win;
+points2result(_) -> lose.
 
 tournament(L1, L2) ->
     lists:sum(lists:zipwith(fun points/2, L1, L2)).
@@ -53,3 +59,35 @@ freq([H|T], R, P, S) ->
         paper    -> freq(T, R, P+1, S);
         scissors -> freq(T, R, P, S+1)
     end.
+
+play(Strategy) ->
+    play(Strategy, [], 0).
+
+play(Strategy, Moves, Score) ->
+    Move = Strategy(Moves),
+    case get_move() of
+        quit ->
+            io:format("You ~p with final score ~b.~n",
+                      [points2result(Score), Score]);
+        YourMove ->
+            R = result(YourMove, Move),
+            S = Score + result2points(R),
+            io:format("You ~p (~p x ~p, ~b so far)~n",
+                      [R, YourMove, Move, S]),
+            play(Strategy, [{YourMove, Move}|Moves], S)
+    end.
+
+get_move() ->
+    get_move(io:get_line("Your move (r,p,s,q): ")).
+
+get_move([$r|_]) -> rock;
+get_move([$R|_]) -> rock;
+get_move([$p|_]) -> paper;
+get_move([$P|_]) -> paper;
+get_move([$s|_]) -> scissors;
+get_move([$S|_]) -> scissors;
+get_move([$q|_]) -> quit;
+get_move([$Q|_]) -> quit;
+get_move(eof)    -> quit;
+get_move({error, _}) -> quit;
+get_move(_)      -> get_move().
